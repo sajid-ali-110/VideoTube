557169617e9fbebc0a05d6eaa314a569248021f3
sajid-ali-110 <sajidaleekhan123123@gmail.com>
2025-08-12 14:02:24 +0500

    Configure Cloudinary and middleware for file uploads

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const userSchema = new mongoose.Schema(
   {
@@ -48,7 +52,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-
 userSchema.pre("save", function (next) {
   if (this.isModified("password")) {
     this.password = bcrypt.hash(this.password, 10);
@@ -56,9 +59,31 @@ userSchema.pre("save", function (next) {
   next();
 });
 
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-// userSchema.methods.isPasswordCorrect = async function (password) {
-//   return await bcrypt.compare(password, this.password);
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
 };
 
 export const User = mongoose.model("User", userSchema);
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { V2 as Cloudinary } from "cloudinary";
+import fs from "fs";
+
+Cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            throw new Error("No file path provided for upload");
+        }
+        // upload the file to Cloudinary
+        const response = await Cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+        // file uploaded successfully, remove the local file
+        return response.secure_url; // Return the URL of the uploaded file
+    } catch (error) {
+        fs.unlinkSync(localFilePath); // Remove the locally saved temporary file after upload operatuion got failed.
+        console.error("Error uploading to Cloudinary:", error);
+        throw new Error("Cloudinary upload failed");
+    }
+}
+export { Cloudinary, uploadCloudinary };
