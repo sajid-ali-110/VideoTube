daf2023a8473c8e89c6981b5c29e1459dd944ff7
sajid-ali-110 <sajidaleekhan123123@gmail.com>
2025-08-13 12:06:15 +0500

    changes in user model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,18 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 // import User from "../models/User.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  //   const { name, email, password } = req.body;
-  //   const user = new User({ name, email, password });
-  //   await user.save();
+  // get all users details from frontend /  request body
+  // validation / user must not be empty
+  // check if user already exists
+  // check for images / avatar
+  // upload them to cloudinary
+  // create user object - create entity in database
+  // remove password and refresh token field from response
+  // check for creation
+
+  const { fullName, username, email, password } = req.body;
+  console.log("email:", email)
+
   res.status(201).json({ message: "User registered successfully" });
 });
 
