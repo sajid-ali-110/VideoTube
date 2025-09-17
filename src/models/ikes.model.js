393e9386618991cc22ce1fac54338381838737a6
sajid-ali-110 <sajidaleekhan123123@gmail.com>
2025-09-17 15:23:32 +0500

    create model fro tweet, likes, comment and playlist

---
+++ src/models/ikes.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema({
+    video: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+        required: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    comment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+    },
+    tweet: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet",
+    },
+    likedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+}, {timestamps: true});
+
+export const Like = mongoose.model("Like", likeSchema);
